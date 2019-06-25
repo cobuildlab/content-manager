523b9e5150fee771304938bdbb9bc2fa8b2cafdf
luis gonzalez <luis.gonzalez@eyss.io>
2019-06-25 18:04:05 -0400

    update style of navbar no finish

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { App } from './App';
 
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/modules/landingpage/components/Navbar.js
+++ src/modules/landingpage/components/Navbar.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import { Row, Button } from '@8base/boost';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 const NavBar = () => {
   return(
-    <div>
-      <nav>
-        <div>
-          <Row justifyContent="start">
-            <Row style={{width: '100%'}}>
-              <Row>
-                <span>LOGIN</span>
-                <Button>SING UP </Button>
-              </Row>
-            </Row>
-          </Row>
+    <Navbar expand="lg">
+      <div className='mr-auto'>
+      <NavbarBrand>
+
+      </NavbarBrand>
+      </div>
+        <div className='ml-auto'>
+           <NavbarToggler aria-controls="navbarNav"/>
         </div>
-      </nav>
-    </div>
-  );
+        <Collapse id="navbarNav" navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem className='mr-2 ml-3'>
+              <NavLink href='#'><span>LOG IN</span></NavLink>
+            </NavItem>
+            <NavItem className='mr-2 ml-3'>
+              <button><span><b>SING UP</b></span></button>
+            </NavItem>
+          </Nav>
+      </Collapse>
+    </Navbar>
+  )
 };
 
 export {NavBar};
