a88eb091f2400c1a2b15cb84d0fadd9cfcc694f8
luis gonzalez <luis.gonzalez@eyss.io>
2019-08-01 17:34:13 -0400

    add style in the dasbhoard

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,15 @@
 import React, {Fragment} from 'react';
-
+import './menu.css'
 const Menu = () => {
 
   return(
     <Fragment>
+      <div className='sidebar'>
+      <i className="fa fa-download"></i>
+ <a href="#services"><i class="fa fa-wrench"></i></a>
+ <a href="#clients"><i class="fa fa-user"></i></a>
+ <a href="#contact"><i class="fa fa-envelope"></i></a>
+      </div>
     </Fragment>
   );
 };
--- src/modules/home/HomeView.js
+++ src/modules/home/HomeView.js
@@ -1,11 +1,14 @@
 import React, {Fragment} from 'react';
 import Header from '../../components/Header'
-
+import Menu from '../../components/Menu'
 const HomeView = () => {
   return(
-    <Fragment>
+    <Fragment >
       <Header />
+      <Menu />
+      <div className='main'>
       Home
+      </div>
     </Fragment>
   )
 }
