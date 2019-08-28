0d64980252e7a8bdf18037bcf77a36812279e67d
Cesar Morales <david_avid1@hotmail.com>
2019-08-28 11:00:04 -0400

    firebase integretion

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_FUSr7pDRWoUzb084n0uQFi1RTL00JUM",
+    authDomain: "content-manager-b5d5d.firebaseapp.com",
+    databaseURL: "https://content-manager-b5d5d.firebaseio.com",
+    projectId: "content-manager-b5d5d",
+    storageBucket: "",
+    messagingSenderId: "738808999341",
+    appId: "1:738808999341:web:327005d42ca455b1"
+  };
+
+  export {firebaseConfig}
